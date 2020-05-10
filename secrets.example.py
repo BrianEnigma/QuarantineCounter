766057867b77fe5b9b5611e93d2ced7122b9fcd2
Brian Enigma <brian@netninja.com>
2020-05-09 17:23:40 -0700

    Secrets file (example)

---
+++ secrets.example.py
@@ -0,0 +1,7 @@
+secrets = {
+    'ssid' : '_your_wifi_ssid',
+    'password : '_your_wifi_password',
+    'aio_username' : '_your_aio_username_',
+    'aio_key' : '_your_big_huge_super_long_aio_key_'
+}
+
